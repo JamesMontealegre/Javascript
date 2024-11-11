ca5c7afec13fc74de23f820bc88d4c073f410f33
WilliamGutierrezMarroquin <wgutierrez@uao.edu.co>
2024-11-11 00:48:05 -0500

    intro

---
+++ Introduction/variables.js
@@ -0,0 +1,11 @@
+// Ambas sirven para declarar variables
+// var es una forma antigua de variables
+// Aun es vigente el uso de var para conservar la compatibilidad con muchos navegadores
+
+var b = 10;
+
+let a = 10,
+  d = 40,
+  e = 50;  
+
+const c = 10;
