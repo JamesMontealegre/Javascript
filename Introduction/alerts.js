a8bc249d1eca9e432b364ecc19ab766d88aceb7e
James Montealegre Gutiérrez <js.montealegre@uniandes.edu.co>
2024-11-12 00:03:29 -0500

    fundamentals

---
+++ Introduction/alerts.js
@@ -0,0 +1,10 @@
+// Métodos del objeto Window
+
+// Popup info
+alert("hola mundo");
+
+// Similar a input en python
+let name = prompt("¿Cual es tu nombre?");
+
+//Boolean
+confirm("¿Esta seguro de continuar?");
